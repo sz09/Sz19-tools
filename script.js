7dab6ac25a49d436f26f119a02b78ca738b173f5
sz09 <37389973+sz09@users.noreply.github.com>
2021-08-17 22:04:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+var hideInput = '#linkToCopy';
 function generate(){
-  alert("generate");
+  var text = $('#inputString').val();
+  if(!text){
+    return;
+  }
+  var value = text;
+  setValueToHideInput(value);
+  copyToClipboard();
 }
 
+function setValueToHideInput(value){
+   $(hideInput).val(value);
+}
+
+funtion copyToClipboard(){
+    $(hideInput).select();      
+    document.execCommand("copy");
+}
